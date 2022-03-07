b3c35906b94c3bb56a990ecaa1358418ac87877b
Md shadab khan <66864511+codewithkhan@users.noreply.github.com>
2022-03-07 21:18:21 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+function App() {
+  var greetings = "";
+  var name = prompt("Please Enter your Name").toUpperCase();
+  var cssStyle = {};
+  var currentTime = new Date().getHours();
+  var text =
+    " This May Look Like a Simple Website But it's Purely React!!! - Components Challenge";
+  if (currentTime >= 1 && currentTime <= 11) {
+    greetings = "Good Morning!";
+    cssStyle.color = "green";
+  } else if (currentTime >= 19 && currentTime <= 23) {
+    greetings = "Good Night!";
+    cssStyle.color = "aqua";
+  } else if (currentTime >= 12 && currentTime <= 18) {
+    greetings = "Good AfterNoon!";
+    cssStyle.color = "cornflowerblue";
+  }
+
+  return (
+    <>
+      <div className="container">
+        <a href="./index.html" target="_khan">
+          <img
+            style={{ padding: "50px", marginTop: "25px" }}
+            src="https://picsum.photos/500/500?grayscale"
+            alt="Error"
+          />
+        </a>
+        <p>
+          {`Hello ${name}`} <span style={cssStyle}>{greetings}</span>
+        </p>
+      </div>
+      <br />
+      <p style={{ paddingLeft: "380px" }}>{text}</p>
+    </>
+  );
+}
+export default App;
